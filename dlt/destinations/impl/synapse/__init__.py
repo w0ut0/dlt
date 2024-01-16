a3abf6d47e208391b1d53ddfd6554f3763c1fec0
Jorrit Sandbrink <sandbj01@heiway.net>
2024-01-16 23:40:55 +0100

    WIP

--- dlt/destinations/impl/synapse/__init__.py
+++ dlt/destinations/impl/synapse/__init__.py
@@ -17,19 +17,30 @@ def capabilities() -> DestinationCapabilitiesContext:
     caps.escape_identifier = escape_postgres_identifier
     caps.escape_literal = escape_mssql_literal
 
-    # Synapse has a max precision of 38: https://learn.microsoft.com/en-us/sql/t-sql/statements/create-table-azure-sql-data-warehouse?view=aps-pdw-2016-au7#DataTypes
+    # Synapse has a max precision of 38
+    # https://learn.microsoft.com/en-us/sql/t-sql/statements/create-table-azure-sql-data-warehouse?view=aps-pdw-2016-au7#DataTypes
     caps.decimal_precision = (DEFAULT_NUMERIC_PRECISION, DEFAULT_NUMERIC_SCALE)
     caps.wei_precision = (DEFAULT_NUMERIC_PRECISION, 0)
     
-    # https://learn.microsoft.com/en-us/sql/sql-server/maximum-capacity-specifications-for-sql-server?view=sql-server-ver16&redirectedfrom=MSDN
+    # https://learn.microsoft.com/en-us/sql/t-sql/statements/create-table-azure-sql-data-warehouse?view=aps-pdw-2016-au7#LimitationsRestrictions
     caps.max_identifier_length = 128
     caps.max_column_identifier_length = 128
-    caps.max_query_length = 4 * 1024 * 64 * 1024
+    
+    # https://learn.microsoft.com/en-us/azure/synapse-analytics/sql-data-warehouse/sql-data-warehouse-service-capacity-limits#queries
+    caps.max_query_length = 65,536 * 4096
     caps.is_max_query_length_in_bytes = True
-    caps.max_text_data_type_length = 2**30 - 1
-    caps.is_max_text_data_type_length_in_bytes = False
+
+    # nvarchar(max) can store 2 GB
+    # https://learn.microsoft.com/en-us/sql/t-sql/data-types/nchar-and-nvarchar-transact-sql?view=sql-server-ver16#nvarchar---n--max--
+    caps.max_text_data_type_length =  2 * 1024 * 1024 * 1024
+    caps.is_max_text_data_type_length_in_bytes = True
+
+    # https://learn.microsoft.com/en-us/azure/synapse-analytics/sql-data-warehouse/sql-data-warehouse-develop-transactions
     caps.supports_transactions = True
-    caps.supports_ddl_transactions = False # https://learn.microsoft.com/en-us/azure/synapse-analytics/sql-data-warehouse/sql-data-warehouse-develop-transactions#limitations
+    caps.supports_ddl_transactions = False
+    
+    # datetimeoffset can store 7 digits for fractional seconds
+    # https://learn.microsoft.com/en-us/sql/t-sql/data-types/datetimeoffset-transact-sql?view=sql-server-ver16
     caps.timestamp_precision = 7
 
     return caps
