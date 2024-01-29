1b6ed763eba96c7e37d4d9fe314b7f0e0550451d
Dave <shrps@posteo.net>
2024-01-29 20:54:06 +0100

    add test pipeline code for sdf integration

---
+++ sdf_test.py
@@ -0,0 +1,20 @@
+import dlt
+
+
+@dlt.resource(name="users", columns={"email": {"classifiers": ["pii.email"]}})
+def resource():
+    yield from [{
+        "name": "dave",
+        "email": "dave@dlthub.com",
+    }, {
+        "name": "marcin",
+        "email": "marcin@dlthub.com",
+    },
+    {
+        "name": "adrian",
+        "email": "adrian@dlthub.com"
+    }]
+
+
+p = dlt.pipeline("sdf_test", destination="duckdb")
+p.run(resource())
