9e184728ad733f77abad0731ff89ac06de644f67
Dave <shrps@posteo.net>
2024-06-20 14:00:11 +0200

    script updates

--- demo.py
+++ demo.py
@@ -2,11 +2,23 @@ import dlt
 import humanize
 import os
 
-def print_nice(p: dlt.Pipeline):
+
+def run(s):
+
+    # run pipeline and print results
+    p = dlt.pipeline(destination="duckdb", pipeline_name="demo", dev_mode=True)
+    p.run(s)
+
     for key, value in p.last_trace.last_normalize_info.row_counts.items():
         if key.startswith("_"):
             continue
         print(f"table {key}: {humanize.intcomma(value)} items")
+
+    # print exraction time
+    print("\n\nExtraction time")
+    print(humanize.precisedelta(p.last_trace.last_extract_info.finished_at - p.last_trace.last_extract_info.started_at))
+    
+
 
 if __name__ == "__main__":
     os.environ["EXTRACT__NEXT_ITEM_MODE"] = "round_robin"
@@ -17,26 +29,19 @@ if __name__ == "__main__":
         @dlt.resource()
         def r1():
             for i in range(1000000):
-                print(f"r1 {i}")
+                # print(f"r1 {i}")
                 yield i
 
         @dlt.resource()
         def r2():
             for i in range(1000000):
-                print(f"r2 {i}")
+                # print(f"r2 {i}")
                 yield i
 
         # add limits
-        r1.add_limit(max_time=1)
-        r2.add_limit(max_time=1)
+        r1.add_limit(max_items=10)
+        r2.add_limit(max_items=10)
 
         return (r1, r2)
 
-    # run pipeline and print results
-    p = dlt.pipeline(destination="duckdb", pipeline_name="demo", dev_mode=True)
-    p.run(source())
-    print_nice(p)
-
-    # print exraction time
-    print("\n\nExtraction time")
-    print(humanize.precisedelta(p.last_trace.last_extract_info.finished_at - p.last_trace.last_extract_info.started_at))
+    run(source())
